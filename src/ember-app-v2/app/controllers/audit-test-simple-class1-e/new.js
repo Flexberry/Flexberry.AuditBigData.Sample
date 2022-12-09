f830878ceb09dc9a7b8cf631ae44a48444822e4c
Nana Appiah <appiah@flexberry.net>
2022-12-09 12:37:45 +0500

    Remove ember-app-v2 files

--- src/ember-app-v2/app/controllers/audit-test-simple-class1-e/new.js
+++
@@ -1,4 +0,0 @@
-import AuditTestSimpleClass1EController from '../audit-test-simple-class1-e';
-
-var AuditTestSimpleClass1ENewController = AuditTestSimpleClass1EController;
-export default AuditTestSimpleClass1ENewController;
--- src/ember-app-v2/app/locales/en/forms/audit-test-simple-class1-l.js
+++
@@ -1,3 +0,0 @@
-export default {
-  caption: 'AuditTestSimpleClass1L'
-};
